b451ed6dce5d1c91488a6914429f6981478a0f1d
vcb <54280546+vcb@users.noreply.github.com>
2020-04-12 19:47:58 +0000

    chat message handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"os/signal"
@@ -15,40 +16,84 @@ type Server struct {
 	mu sync.Mutex
 	wg sync.WaitGroup
 
-	stop    chan struct{}
-	port    string
-	clients map[string]net.Conn // TODO
+	stop     chan struct{}
+	port     string
+	lastClientID int
+	clients  map[string]*Client
+	channels map[uint16]*Channel
+	out chan Msg // outgoing messages
+}
+
+func NewServer(port string) *Server {
+	return &Server{
+		port:     port,
+		stop:     make(chan struct{}),
+		clients:  make(map[string]*Client),
+		channels: make(map[uint16]*Channel),
+	}
 }
 
 type Client struct {
 	UserID uint16 `json:"userId"`
 	Name   string `json:"name"`
+
+	conn net.Conn
 }
+
+func (s *Server) NewClient(conn net.Conn) *Client {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	c := &Client{
+		UserID: uint16(s.lastClientID),
+		Name: "teme", // TODO
+		conn: conn,
+	}
+	s.lastClientID++
+
+	return c
+}
+
+type ChannelType uint8
+
+const (
+	NoneChannelType ChannelType = iota // TODO this would be a category
+	TextChannelType
+	VoiceChannelType
+	MultiChannelType // text + voice
+)
 
 type Channel struct {
-	ChannelID uint16 `json:"channelId"`
-	ParentID  uint   `json:"parentId"`
-	Name      string `json:"name"`
-	Type      uint8  `json:"type"`
+	ID uint16      `json:"channelId"`
+	ParentID  uint16      `json:"parentId"`
+	Name      string      `json:"name"`
+	Type      ChannelType `json:"type"`
+
+	mu            sync.RWMutex
+	lastMessageID int
+	messages      map[uint16]*Message
 }
 
-type Message struct {
-	MessageId  uint16 `json:"messageId"`
-	SenderID   uint16 `json:"senderId"`
-	SenderName uint16 `json:"senderName"`
-	Content    string `json:"content"`
-}
-
-func NewServer(port string) *Server {
-	return &Server{
-		port:    port,
-		stop:    make(chan struct{}),
-		clients: make(map[string]net.Conn),
+func NewChannel(channelType ChannelType) *Channel {
+	// TODO
+	return &Channel{
+		ID: 0,
+		ParentID:  0,
+		Name:      "txt",
+		Type:      channelType,
+		messages:  make(map[uint16]*Message),
 	}
 }
 
+type Message struct {
+	MessageID  uint16 `json:"messageId"`
+	SenderID   uint16 `json:"senderId"`
+	SenderName string `json:"senderName"`
+	Content    string `json:"content"`
+}
+
 func (s *Server) Start() {
-	s.wg.Add(1)
+	s.wg.Add(3) // XXX TODO
 	go s.tcpServer()
 
 	log.Info("Starting Etcord server")
@@ -72,10 +117,33 @@ func (s *Server) tcpServer() {
 	log.Infof("Server listening on %s", s.port)
 
 	go func() {
+		defer s.wg.Done()
 		select {
 		case <-s.stop:
 			l.Close()
 			return
+		}
+	}()
+
+	// TODO
+	go func() {
+		defer s.wg.Done()
+		for {
+			select {
+			case <-s.stop:
+				return
+			case m := <-s.out:
+				for _, client := range s.clients {
+					b, err := Serialize(m)
+					if err != nil {
+						log.Errorf("Failed to serialize outgoing message: %s", err)
+						break
+					}
+					if _, err := client.conn.Write(b); err != nil {
+						s.removeClient(client)
+					}
+				}
+			}
 		}
 	}()
 
@@ -92,15 +160,16 @@ func (s *Server) tcpServer() {
 	}
 }
 
-func (s *Server) handleConn(c net.Conn) {
-	log.Infof("Connected to %s", c.RemoteAddr().String())
+func (s *Server) handleConn(conn net.Conn) {
+	log.Infof("Connected to %s", conn.RemoteAddr().String())
 
+	c := s.NewClient(conn)
 	s.addClient(c)
 	defer s.removeClient(c)
 
 	tmp := make([]byte, 1024)
 	for {
-		n, err := c.Read(tmp)
+		n, err := conn.Read(tmp)
 		if err != nil {
 			log.Error(err)
 			break
@@ -122,38 +191,79 @@ func (s *Server) handleConn(c net.Conn) {
 		}
 
 		for _, m := range msgs {
-			s.msgHandler(m)
+			// TODO error handling
+			if err := s.msgHandler(m); err != nil {
+				log.Errorf("Failed to process msg: %s", err)
+			}
 		}
 	}
 
-	if err := c.Close(); err != nil {
+	if err := conn.Close(); err != nil {
 		log.Errorf("Failed to close connection: %s", err)
 	}
+
+	log.Infof("Disconnected from %s", conn.RemoteAddr().String())
 }
 
-func (s *Server) msgHandler(m Msg) {
-	log.Infof("Recv msg: %s", m)
-}
-
-func (s *Server) addClient(c net.Conn) {
+func (s *Server) addClient(c *Client) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.clients[c.RemoteAddr().String()] = c
+	s.clients[c.conn.RemoteAddr().String()] = c
 }
 
-func (s *Server) removeClient(c net.Conn) {
+func (s *Server) removeClient(c *Client) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	delete(s.clients, c.RemoteAddr().String())
+	delete(s.clients, c.conn.RemoteAddr().String())
+}
+
+func (s *Server) msgHandler(m Msg) error {
+	log.Debugf("Recv msg: %s", m)
+
+	var err error
+	switch m.(type) {
+	case *ChatMessageRequest:
+		err = s.handleChatMessage(m.(*ChatMessageRequest))
+	}
+
+	return err
+}
+
+func (s *Server) handleChatMessage(req *ChatMessageRequest) error {
+	chn, ok := s.channels[req.ChannelID]
+	if !ok {
+		return fmt.Errorf("channel with ID %d does not exist", req.ChannelID)
+	}
+
+	chn.mu.Lock()
+	defer chn.mu.Unlock()
+
+	chn.lastMessageID++
+	m := &Message{
+		MessageID: uint16(chn.lastMessageID),
+		Content:   req.Content,
+	}
+	chn.messages[m.MessageID] = m
+
+	res := &ChatMessageResponse{
+		ChannelID: chn.ID,
+		Message: *m,
+	}
+
+	s.out <- Msg(res)
+
+	return nil
 }
 
 func main() {
+	// TODO config
 	if len(os.Args) != 2 {
 		log.Error("Missing port number")
 		return
 	}
+	log.SetLevel(log.TraceLevel)
 
 	s := NewServer(os.Args[1])
 	s.Start()
@@ -175,8 +285,4 @@ func main() {
 	}()
 
 	s.wg.Wait()
-}
-
-func init() {
-	log.SetLevel(log.TraceLevel)
 }
